a86bfe125b4919e96a15337810ca7267ff6ac365
meplicatoin <yuuki_s@naver.com>
2022-10-08 03:40:38 +0900

    update common

--- common/common.js
+++ common/common.js
@@ -10,3 +10,14 @@ $(function() {
 function extNumber(res) {
   return Number(res.replace(/[^0-9]/g, ""));
 }
+
+// 시간날짜분리
+function getDatebyString(date) {
+  return {
+    year: date.substr(0, 4),
+    month: date.substr(4, 2),
+    day: date.substr(6, 2),
+    hour: date.substr(8, 2),
+    min: date.substr(10, 2),
+  };
+}
