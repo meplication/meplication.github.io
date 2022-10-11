1ed0e91c4b5336970b224e81d3b0118dc9fd239b
meplicatoin <yuuki_s@naver.com>
2022-10-11 19:17:49 +0900

    update weeklyEvent

--- todo/list/weeklyEvent.js
+++ todo/list/weeklyEvent.js
@@ -6,11 +6,4 @@ export const weeklyEventList = [
     startDate: "202209291000",
     endDate: "202210262359",
   },
-  {
-    id: "hangeul_2022",
-    icon: "./img/wEvent/hangeul.png",
-    title: "훈민정음 생일을 축하해",
-    startDate: "202210071000",
-    endDate: "202210262359",
-  },
 ];
